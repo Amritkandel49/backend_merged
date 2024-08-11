807dbca012b5d889dc91a36ce78e7ed77db5ad0e
Amritkandel49 <amritkandel062@gmail.com>
2024-08-11 20:41:27 +0545

    ascd

--- src/app.js
+++ src/app.js
@@ -43,10 +43,10 @@ app.use(
 );
 
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "..", "dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 import ownerRouter from "./routes/owner.route.js";
